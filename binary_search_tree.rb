b763be8f753d6450cd721d8487798a7215001688
Mark R <mirubenstein@gmail.com>
2018-01-14 10:52:40 -0800

    Update based on review and checking others code. Still want to balance tree and use polymorphism and other basic operations for bst.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,41 +1,36 @@
 class Bst
-  attr_reader :data, :left, :right
+  include Enumerable
+
+  attr_reader :data, :size
 
   def initialize(data)
     @data = data
+    @size = 1
   end
 
   def each(&block)
-    return enum_for(__method__) unless block_given?
-    left.each(&block) if left
+    return enum_for(__method__) { @size } unless block_given?
+    left&.each(&block)
     yield data
-    right.each(&block) if right
+    right&.each(&block)
+    self
   end
 
   def insert(other)
     if other <= data
-      insert_left(other)
+      @left = Bst.new(other) unless @left&.insert(other)
     else
-      insert_right(other)
+      @right = Bst.new(other) unless @right&.insert(other)
     end
+    @size += 1
   end
 
-  private
-
-  def insert_left(other)
-    if left.nil?
-      @left = Bst.new other
-    else
-      left.insert other
-    end
+  def left
+    @left.dup.freeze
   end
 
-  def insert_right(other)
-    if right.nil?
-      @right = Bst.new other
-    else
-      right.insert other
-    end
+  def right
+    @right.dup.freeze
   end
 end
 
@@ -43,3 +38,14 @@ end
 module BookKeeping
   VERSION = 1
 end
+
+# If you want a good challenge, than try implementing a BST with object type polymorphism
+# to get rid of the explicit branching entirely.
+#   I am not sure if it would lead to great code here, but it's an interesting exercise in OO.
+#
+# A BST is really only useful to do fast queries.
+# If you like a little challenge, consider adding the missing basic operations on BSTs.
+#
+# Your tree is not balanced.
+# I think the tests are all balanced,
+# so you could try to make the tree self-balancing.
